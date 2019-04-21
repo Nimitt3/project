3b0473bcbb5bbb6825238c6543fc74148821d025
Nimitt3 <nimittchauhan21@gmail.com>
2019-04-21 09:26:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/login', (req, res) => res.json({'name' : 'Being Zero', 'age' : '2 years'}))
 
-app.get('/loginnow', (req, res) => res.send('index.html'));
+app.get('/loginnow', (req, res) => res.send(__dirname+'/index.html'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
